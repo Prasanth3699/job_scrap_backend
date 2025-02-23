ffb5d6bb32098b9460af23fdbbb1682bcca4deaf
Prasanth <prasanthranger09@gmail.com>
2025-02-23 18:34:46 +0530

    selenium driver issue code update

--- main.py
+++ main.py
@@ -17,6 +17,11 @@ ORIGINS = [
     if origin.strip()
 ]
 
+origins = [
+    "https://job-scrap-ckpdxhkvy-prasanths-projects-1c782ce1.vercel.app",
+    "http://localhost:3000",
+]
+
 
 def get_application():
     db = next(get_db())
@@ -28,7 +33,7 @@ def get_application():
     # Configure CORS
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=ORIGINS,  # Allow requests from your Next.js app
+        allow_origins=origins,  # Allow requests from your Next.js app
         allow_credentials=True,
         allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
         allow_headers=["*"],  # Allow all headers
@@ -60,56 +65,3 @@ def get_application():
 
 
 app = get_application()
-
-
-# from fastapi import FastAPI
-# from app.core.config import get_settings
-# from app.services.scheduler_service import init_scheduler, scheduler
-# from app.api.router import api_router
-# from app.db.base import Base
-# from app.db.session import engine
-# from app.core.logger import logger
-
-# settings = get_settings()
-
-
-# # Create database tables
-# def init_db():
-#     try:
-#         Base.metadata.create_all(bind=engine)
-#         logger.info("Database tables created successfully")
-#     except Exception as e:
-#         logger.error(f"Error creating database tables: {e}")
-#         raise
-
-
-# app = FastAPI(title=settings.APP_NAME)
-
-# # Include API router
-# app.include_router(api_router)
-
-
-# @app.on_event("startup")
-# async def startup_event():
-#     """Initialize services on startup"""
-#     try:
-#         logger.info("Starting up the application...")
-#         init_db()  # Initialize database
-#         init_scheduler()
-#         scheduler.start()
-#         logger.info("Application startup completed successfully")
-#     except Exception as e:
-#         logger.error(f"Error during startup: {str(e)}")
-#         raise
-
-
-# @app.on_event("shutdown")
-# async def shutdown_event():
-#     """Cleanup on shutdown"""
-#     try:
-#         logger.info("Shutting down the application...")
-#         scheduler.shutdown()
-#         logger.info("Application shutdown completed successfully")
-#     except Exception as e:
-#         logger.error(f"Error during shutdown: {str(e)}")
-#         raise
